6c5922c73884fabca31f16c24c6659a711d54800
moonsoo20 <98132695+moonsoo20@users.noreply.github.com>
2022-12-27 09:45:38 +0900

    12/27

---
+++ src/Ex02_01_산술연산자.java
@@ -0,0 +1,14 @@
+
+public class Ex02_01_산술연산자 {
+	public static void main(String[] args) {
+		int num1, num2;
+		
+		num1=30;
+		num2=7;
+		System.out.println(num1+"+"+num2+"="+(num1+num2));
+		System.out.println(num1+"-"+num2+"="+(num1-num2));
+		System.out.println(num1+"*"+num2+"="+(num1*num2));
+		System.out.printf("%d/%d=%.2f",num1,num2,(double)num1/num2);
+		
+	}
+}
